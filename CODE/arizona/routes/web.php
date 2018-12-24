479335c9fd0a8ee755225dde394dbae24903f1ff
Sheshank <sheshank.jamwal@debugged-pro.com>
2018-12-24 19:51:33 +0530

    routing

--- CODE/arizona/routes/web.php
+++ CODE/arizona/routes/web.php
@@ -9,57 +9,37 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('home.login');
 });
 
-
-
 Auth::routes();
 
 Route::group(['middleware' => 'auth'], function () {
 
-Route::get('/dashboard','DashboardController@index')->name('dashboard');
+    Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
 
-
-Route::get('/companies', 'CompaniesController@index');
-Route::get('/addcompanies', 'CompaniesController@create');
-Route::post('/savecompanies', 'CompaniesController@store');
-Route::get('/companies/edit/{id}', 'CompaniesController@edit');
-Route::post('/companies/update/{id}', 'CompaniesController@update');
-Route::get('/companies/delete/{id}', 'CompaniesController@destroy');
-
-
-Route::get('/projects', 'ProjectsController@index');
-Route::get('/addprojects', 'ProjectsController@create');
-Route::post('/saveprojects', 'ProjectsController@store');
-Route::get('/projects/edit/{id}', 'ProjectsController@edit');
-Route::post('/projects/update/{id}', 'ProjectsController@update');
-Route::get('/projects/delete/{id}', 'ProjectsController@destroy');
-
+    Route::get('/companies', 'CompaniesController@index');
+    Route::get('/addcompanies', 'CompaniesController@create');
+    Route::post('/savecompanies', 'CompaniesController@store');
+    Route::get('/companies/edit/{id}', 'CompaniesController@edit');
+    Route::post('/companies/update/{id}', 'CompaniesController@update');
+    Route::get('/companies/delete/{id}', 'CompaniesController@destroy');
 
 // Employees Routes
-\App\Http\Controllers\EmployeesController::routes();
+    \App\Http\Controllers\EmployeesController::routes();
 
 // JobPosts Routes
-\App\Http\Controllers\JobPostsController::routes();
+    \App\Http\Controllers\JobPostsController::routes();
 
-Route::get('/departments', 'DepartmentsController@index');
-Route::get('/adddepartments', 'DepartmentsController@create');
-Route::post('/savedepartments', 'DepartmentsController@store');
-Route::get('/departments/edit/{id}', 'DepartmentsController@edit');
-Route::post('/departments/update/{id}', 'DepartmentsController@update');
-Route::get('/departments/delete/{id}', 'DepartmentsController@destroy');
+// Project Routes
+    \App\Http\Controllers\ProjectsController::routes();
 
+// Departments Routes
+    \App\Http\Controllers\DepartmentsController::routes();
 
-Route::get('/stations', 'StationsController@index');
-Route::get('/addstations', 'StationsController@create');
-Route::post('/savestations', 'StationsController@store');
-Route::get('/stations/edit/{id}', 'StationsController@edit');
-Route::post('/stations/update/{id}', 'StationsController@update');
-Route::get('/stations/delete/{id}', 'StationsController@destroy');
-
-
+//Stations Routes
+    \App\Http\Controllers\StationsController::routes();
 });
