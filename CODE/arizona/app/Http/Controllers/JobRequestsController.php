abcf9c5d09d1bcac431fd79a7b8d232c8d7b3dd3
bkanwasi <bkanwasi21@gmail.com>
2018-12-25 17:17:15 +0530

    update  job request

--- CODE/arizona/app/Http/Controllers/JobRequestsController.php
+++ CODE/arizona/app/Http/Controllers/JobRequestsController.php
@@ -20,8 +20,8 @@ class JobRequestsController extends Controller
      */
     public function index()
     {
-
-        $list = jobRequests::where(['status' => 1])->paginate(10);
+        $user_id = Auth::id();
+        $list = jobRequests::where(['status' => 1,'user_id'=>$user_id])->paginate(10);
         return view('hrmodule.jobrequest.list')->with([
             'listData' => $list,
             'pageTitle' => "jobrequest",
